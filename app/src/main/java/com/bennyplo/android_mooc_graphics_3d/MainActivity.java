e47d6c60120be14fbd2b6a264c90265bee6d3367
Wessel W. Bakker <wwbakker@gmail.com>
2021-06-05 10:16:05 +0200

    - Update versions of gradle and project dependencies

--- app/src/main/java/com/bennyplo/android_mooc_graphics_3d/MainActivity.java
+++ app/src/main/java/com/bennyplo/android_mooc_graphics_3d/MainActivity.java
@@ -1,7 +1,8 @@
 package com.bennyplo.android_mooc_graphics_3d;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private MyView mMyView=null;//a custom view for drawing
